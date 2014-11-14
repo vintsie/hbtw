b3d62761cecc6462a80cc06ba4c55b514d02033f
Vin Tsie <vin.tsie@hotmail.com>
2014-11-14 14:52:49 +0800

    commit for checkout branch

--- src/main/java/com/inftt/example/mongodb/MongoDBInstance.java
+++ src/main/java/com/inftt/example/mongodb/MongoDBInstance.java
@@ -7,6 +7,7 @@ import com.mongodb.ServerAddress;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Mongodb client singleton.
@@ -14,6 +15,8 @@ import java.util.List;
  * Created by Vin on 2014/10/14.
  */
 public class MongoDBInstance {
+
+    static Logger logger = Logger.getLogger(MongoDBInstance.class.getName());
 
     public final static String HOST = "127.0.0.1";
     public final static int PORT = 27017;
--- src/main/java/com/inftt/example/mongodb/MongoDBTest.java
+++ src/main/java/com/inftt/example/mongodb/MongoDBTest.java
@@ -1,6 +1,7 @@
 package com.inftt.example.mongodb;
 
 import com.mongodb.*;
+import org.junit.Test;
 
 
 /**
@@ -17,14 +18,46 @@ public class MongoDBTest {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 10; i++) {
-            Thread.sleep(2000);
-            DBCollection dbc = MongoDBInstance.getMongoDB(MDB_N.CLOUD1).getCollection("testData");
-            DBCursor cursor = dbc.find();
-            while (cursor.hasNext()) {
-                DBObject object = cursor.next();
-                System.out.println(object.toString());
-            }
-        }
+        //Thread.sleep(2000);
+        DBCollection dbc = MongoDBInstance.getMongoDB(MDB_N.CLOUD1).getCollection("testData");
+        dbc.drop();
+        //DBCursor cursor = dbc.find();
+        //while (cursor.hasNext()) {
+            //DBObject object = cursor.next();
+            //String objectId = String.valueOf(object.get("_id"));
+            //dbc.remove(object);
+            //System.out.println("Object [" + objectId + "] removed.");
+        //}
+        //DBObject object = new BasicDBObject();
+        //object.put("dept_id", "D90021");
+        //object.put("dept_name", "SECURE DEP.");
+        //object.put("master", "Henrik Rails");
+        //dbc.insert(object);
+    }
+
+    @Test
+    public void testRichObjectInsertion() throws Exception {
+        DBObject object = new BasicDBObject("title", "Purchase Order 1").append("total", 10.50);
+        DBObject lineItem = new BasicDBObject("sku", "a").append("quantity", 1).append("price", 10.50);
+        BasicDBList dbList = new BasicDBList();
+        dbList.add(lineItem);
+
+        object.put("line_items", dbList);
+        //MongoDBInstance.getMongoDB(MDB_N.CLOUD1).createCollection("purchase_list", null);
+        MongoDBInstance.getMongoDB(MDB_N.CLOUD1).getCollection("purchase_list").insert(object);
+    }
+
+    @Test
+    public void testModifyDBObject() throws Exception{
+        //DBCursor cursor = MongoDBInstance.getMongoDB(MDB_N.CLOUD1)
+        //        .getCollection("purchase_list").find(new BasicDBObject("title", "Purchase Order 1"));
+
+        DBCollection collection = MongoDBInstance.getMongoDB(MDB_N.CLOUD1).getCollection("purchase_list");
+        collection.update(new BasicDBObject("title", "Purchase Order 2"),
+                new BasicDBObject("$set", new BasicDBObject("title", "Purchase Order 3")));
+
+        //while(cursor.hasNext()){
+        ///    System.out.println(cursor.next());
+        //}
     }
 }
